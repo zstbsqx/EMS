3a56e92886ddfa405a9b5feb74486da78fa1af03
zstbsqx <chengji_001@163com>
2015-08-26 23:22:30 +0800

    remove debug info

--- ems/actions/LoginAction.py
+++ ems/actions/LoginAction.py
@@ -10,13 +10,11 @@ class LoginAction(ActionBase):
             return 'You have logged in'
         name = self.checkForm('name', not_empty=True)
         password = self.checkForm('password', not_empty=True)
-        print('name:%s password:%s' % (name, password))
         m = hashlib.md5()
         m.update(password)
         md5Password = m.hexdigest()
         users = UserDao.UserDao.queryLoginUser(name, md5Password)
         if len(users) is 1:
-            print(users[0])
             uid = users[0]['user_id']
             session['uid'] = uid
             return 'Login success'
